434a968db08ef690ca43aef2f318313b61b981d6
Andrew Walters <ajw222@vt.edu>
2017-04-24 15:33:43 -0400

    Added a fuckton
    Added several images and edited index.html to take advantage of them.
    Also added better blurb at the top of index.html.

--- email.php
+++ email.php
@@ -32,8 +32,7 @@ $headers .= "Reply-To: $visitor_email \r\n";
 //Send the email!
 mail($to,$email_subject,$email_body,$headers);
 //done. redirect to thank-you page.
-header("Refresh:0");
-
+header("https://thefalcon222.github.io/"); /* Redirect browser */
 
 // Function to validate against any email injection attempts
 function IsInjected($str)
